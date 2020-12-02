18d945d234dc292d2f21c92927693e64e0038a11
clherrington <colin.herrington330@gmail.com>
2020-12-02 14:46:32 -0500

    the beginnings

---
+++ View/navbar.php
@@ -0,0 +1,5 @@
+<div class= 'navbar-container'>
+        <div><h3 class = 'h3-nav'>Home</h3></div>
+        <div><h3 class = 'h3-nav'>Projects</h3></div>
+        <div><h3 class = 'h3-nav'>About Me</h3></div>
+</div>
